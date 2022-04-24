7c2e4e6b1160cb386292aa1aaf1f48851ba1dede
florence ajadi <florence.ajadi@yahoo.com>
2022-04-24 14:46:36 -0500

    commit tree 046a9fb59d3d0b783b37aff3fb1599cea43b9bf5

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2 
+
+
+const random = Math.floor(Math.random()*4)*1;
+
+console.log(random);
+
+const num3 = 16
+const num4 = 4
+const mod = num3 / num4
+
+let max = Math.max(1, 2, 3, 4, 20) 
+
 
 // Write your code here
